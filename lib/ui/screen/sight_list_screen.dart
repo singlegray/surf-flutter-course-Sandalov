043bb940964f0f8c9ba6d46917300409312bd293
singlegray <singlegray@gmail.com>
2021-01-11 21:52:15 +0300

    Create sight_list_screen.dart

---
+++ lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  @override
+  createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
