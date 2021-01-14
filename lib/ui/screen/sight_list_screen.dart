dd3606b26d1b972c14e67609b516a22dd775a1d6
singlegray <singlegray@gmail.com>
2021-01-14 22:09:50 +0300

    Update sight_list_screen.dart

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,6 +8,31 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60.0),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: RichText(
+            text: TextSpan(
+                text: 'C',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontFamily: 'Roboto-Regular.ttf',
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                      text: 'писок\n', style: TextStyle(color: Colors.black)),
+                  TextSpan(text: 'и', style: TextStyle(color: Colors.yellow)),
+                  TextSpan(
+                      text: 'нтересных мест',
+                      style: TextStyle(color: Colors.black)),
+                ]),
+          ),
+        ),
+      ),
+    );
   }
 }
