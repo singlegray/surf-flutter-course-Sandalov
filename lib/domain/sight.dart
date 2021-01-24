ee4b1201ccb19b129cacb265308be925d657aef4
singlegray <singlegray@gmail.com>
2021-01-24 14:44:51 +0300

    Create sight.dart

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  String name;
+  int lat;
+  int lon;
+  String url;
+  String details;
+  String type;
+
+  Sight(this.name, this.lat, this.lon, this.url, this.details, this.type);
+}
