6a4b288a0c1e6b888b8411b5f4571e72ddfdc6fe
singlegray <singlegray@gmail.com>
2021-02-07 20:18:41 +0300

    Update mocks.dart

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -1,21 +1,36 @@
 import 'package:places/domain/sight.dart';
 
-class Mock {
-  final List mocks = [];
-
-  Mock() {
-    Sight oneplace = Sight('One', 1, 1, 'url1', 'det', 'ty');
-    Sight twoplace = Sight('One2', 2, 1, 'url2', 'det', 'ty');
-    Sight freplace = Sight('One3', 3, 1, 'url3', 'det', 'ty');
-    Sight forplace = Sight('One4', 4, 1, 'url4', 'det', 'ty');
-    Sight fitheplace = Sight('One5', 5, 1, 'url5', 'det', 'ty');
-    Sight sixplace = Sight('One6', 6, 1, 'url6', 'det', 'ty');
-
-    mocks.add(oneplace);
-    mocks.add(twoplace);
-    mocks.add(freplace);
-    mocks.add(forplace);
-    mocks.add(fitheplace);
-    mocks.add(sixplace);
-  }
-}
+final List mocks = [
+  Sight(
+    "Сад «Эрмитаж»",
+    1,
+    1,
+    "https://kudago.com/media/thumbs/xl/images/place/95/62/9562b12ccea1409dbb3cb14e3c596969.jpg",
+    "Стоит на месте старой усадьбы",
+    "Усадьба",
+  ),
+  Sight(
+    "Смотровая площадка РАН",
+    1,
+    1,
+    "https://kudago.com/media/thumbs/xl/images/place/f8/d7/f8d76d25bc89afdbc2b2cc13d89cf3eb.jpg",
+    "С площадки открывается красивый вид",
+    "Площадка",
+  ),
+  Sight(
+    "Центр дизайна Artplay",
+    1,
+    1,
+    "https://kudago.com/media/thumbs/xl/images/place/f6/1c/f61c1b3a93096585599b014a3830c20b.jpg",
+    "Место для отдыха и работы",
+    "Пространство",
+  ),
+  Sight(
+    "Музей космонавтики",
+    1,
+    1,
+    "https://kudago.com/media/thumbs/xl/images/place/5f/59/5f59c4548df1db57c592e38702de430c_3.jpg",
+    "Музей космонавтики",
+    "Музей",
+  )
+];
